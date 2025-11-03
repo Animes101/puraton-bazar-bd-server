0d8751880c9b939ec797de77da3cb45a16134b8b
Animes <animesbarman101@gmail.com>
2025-11-03 15:29:56 +0600

    add products collection

--- index.js
+++ index.js
@@ -28,12 +28,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //products relate api
+
+   const PuratonBazar = client.db('PuratonBazar');
+const products = PuratonBazar.collection('products');
+
+app.get('/products', async (req, res) => {
+  try {
+    const result = await products.find().toArray();
+    res.status(200).json({ status: 'ok', data: result });
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ status: 'error', message: 'Server problem' });
+  }
+});
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
