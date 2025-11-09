bf1088462fa0eadb3f9739e262328dbe9e74e789
Animes <animesbarman101@gmail.com>
2025-11-09 19:52:23 +0600

    make admin

--- index.js
+++ index.js
@@ -191,6 +191,27 @@ async function run() {
 
     })
 
+      app.patch("/users/:id", async (req, res)=>{
+       try {
+        const {id}=req.params;
+        const query={_id:new ObjectId(id)};
+
+        const updateDoc={
+          $set:{role:"admin"}
+        }
+
+        const result=await usersCollection.updateOne(query, updateDoc);
+
+        if (result) {
+          res.status(200).json({ status: "ok", data: result });
+        }
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        res.status(500).json({ status: "error", message: "Server problem" });
+      }
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
