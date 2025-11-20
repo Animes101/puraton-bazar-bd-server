3018c5e7d070f44fa8c8330323384f0f5f6e749a
Animes <animesbarman101@gmail.com>
2025-11-20 19:28:38 +0600

    add some

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const SSLCommerzPayment = require('sslcommerz-lts')
+const SSLCommerzPayment = require("sslcommerz-lts");
 
 app.use(express.json());
 app.use(cors());
 const PORT = 3000;
 
-const store_id = 'midla68ef5f2b0cf63';
-const store_passwd = 'midla68ef5f2b0cf63@ssl';
-const is_live = false; 
+const store_id = "midla68ef5f2b0cf63";
+const store_passwd = "midla68ef5f2b0cf63@ssl";
+const is_live = false;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello from Express Server!................");
@@ -37,84 +37,91 @@ async function run() {
     //jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(
-       user,
-        process.env.SECRITE_TOKEN,
-        { expiresIn: "1h" }
-      );
+      const token = jwt.sign(user, process.env.SECRITE_TOKEN, {
+        expiresIn: "1h",
+      });
 
-      res.json({token: token});
+      res.json({ token: token });
     });
 
     //middleware for verify jwt
-    const verifyToken=(req,res,next)=>{
+    const verifyToken = (req, res, next) => {
+      const authorization = req.headers.authorization;
 
-      const authorization=req.headers.authorization;
-
-      if(!authorization){
-        
-        return res.status(401).send({error:true, message:'unauthorized access'})
+      if (!authorization) {
+        return res
+          .status(401)
+          .send({ error: true, message: "unauthorized access" });
       }
-      const token=authorization.split(' ')[1];
+      const token = authorization.split(" ")[1];
 
-                // verify a token symmetric
-          jwt.verify(token, process.env.SECRITE_TOKEN, (err, decoded) =>{
-            
-            
-            if(err){
-              return res.status(401).send({error:true, message:'unauthorized access'})
-            }
-            req.decoded=decoded;
-            next()
+      // verify a token symmetric
+      jwt.verify(token, process.env.SECRITE_TOKEN, (err, decoded) => {
+        if (err) {
+          return res
+            .status(401)
+            .send({ error: true, message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
-          });
+    //middleware for verify Admin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
 
-    }
+      const query = { email: email };
 
-
-      //middleware for verify Admin
-    const verifyAdmin= async (req, res, next)=> {
-
-
-      const email=req.decoded.email; 
-
-      const query={email:email}
-
-      const user=await usersCollection.findOne(query);
-      if(user?.role !== 'admin'){
-        return res.status(403).send({error:true, message:'forbidden access'})
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
 
       next();
-    }
+    };
 
     //products relate api
 
     const PuratonBazar = client.db("PuratonBazar");
     const products = PuratonBazar.collection("products");
 
-    app.get("/products", verifyToken , async (req, res) => {
-      try {
+   app.get("/products", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 0;
+    const limit = parseInt(req.query.limit) || 10;
 
-        const page=parseInt(req.query.page);
-        const limit=parseInt(req.query.limit);
+    
+    const result = await products
+      .find()
+      .skip(page * limit)
+      .limit(limit)
+      .toArray();
+      
 
-        
-        const result = await products.find().skip(page * limit).limit(limit).toArray();
+    const total_product = await products.countDocuments();
 
-        const total_product= await products.estimatedDocumentCount();
-        res.status(200).json({ status: "ok", data: result, total_product:total_product });
-      } catch (error) {
-        console.error("Error fetching products:", error);
-        res.status(500).json({ status: "error", message: "Server problem" });
-      }
+    res.status(200).json({
+      status: "ok",
+      data: result,
+      total_product: total_product,
     });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Server problem",
+    });
+  }
+});
 
 
-     app.delete("/products/:id", verifyToken, verifyAdmin , async (req, res) => {
+    app.delete("/products/:id", verifyToken, verifyAdmin, async (req, res) => {
       try {
-        const {id}=req.params;
-        const query={_id:new ObjectId(id)}
+        const { id } = req.params;
+        const query = { _id: new ObjectId(id) };
 
         const result = await products.deleteOne(query);
 
@@ -154,54 +161,47 @@ async function run() {
       }
     });
 
-     app.post("/products", verifyToken, verifyAdmin, async (req, res) => {
+    app.post("/products", verifyToken, verifyAdmin, async (req, res) => {
       try {
         const newItem = req.body;
 
-        const result= await products.insertOne(newItem);
+        const result = await products.insertOne(newItem);
 
-        if(result.insertedId){
-
-          res.status(200).json({ status: "ok", data: result })
+        if (result.insertedId) {
+          res.status(200).json({ status: "ok", data: result });
         }
-
-        
-
       } catch (error) {
         console.error("Error fetching product:", error);
         res.status(500).json({ status: "error", message: "Server problem" });
       }
     });
 
-     app.patch("/Products/:id", async (req, res) => {
+    app.patch("/Products/:id", async (req, res) => {
       try {
         const { id } = req.params;
 
-        const newItem=req.body;
+        const newItem = req.body;
 
-        const query={_id: new ObjectId(id)}
-
-
-       
-        
+        const query = { _id: new ObjectId(id) };
 
         const updateDoc = {
-          $set: { 
-           
+          $set: {
             category: newItem.category,
             name: newItem.name,
             brand: newItem.brand,
             price: newItem.price,
             condition: newItem.condition,
             description: newItem.description,
-            images: [newItem?.images?.[0] || null, newItem?.images?.[1] || null],
+            images: [
+              newItem?.images?.[0] || null,
+              newItem?.images?.[1] || null,
+            ],
             postedAt: newItem.postedAt,
-       
           },
         };
 
         const result = await products.updateOne(query, updateDoc);
-        
+
         if (result) {
           res.status(200).json({ status: "ok", data: result });
         }
@@ -268,7 +268,7 @@ async function run() {
 
     //Users related api
     const usersCollection = PuratonBazar.collection("users");
-    app.post("/users", async  (req, res) => {
+    app.post("/users", async (req, res) => {
       try {
         const userInfo = req.body;
 
@@ -297,8 +297,8 @@ async function run() {
       try {
         const { email } = req.params;
 
-        if(email !== req.decoded.email){
-          return res.status(403).json({status:'forbidden access'})
+        if (email !== req.decoded.email) {
+          return res.status(403).json({ status: "forbidden access" });
         }
         const query = { email: email };
         const user = await usersCollection.findOne(query);
@@ -315,9 +315,6 @@ async function run() {
 
     app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       try {
-      
-
-       
         const result = await usersCollection.find().toArray();
 
         if (result) {
@@ -365,213 +362,186 @@ async function run() {
       }
     });
 
-    //payment getWay api 
+    //payment getWay api
 
-    const paymentCollection=PuratonBazar.collection('payment')
+    const paymentCollection = PuratonBazar.collection("payment");
 
-      app.post("/order", async (req, res) => {
-        try {
-          const order = req.body;
+    app.post("/order", async (req, res) => {
+      try {
+        const order = req.body;
 
         // Create unique transaction ID
-          const tran_id = "TXN_" + Date.now();
+        const tran_id = "TXN_" + Date.now();
 
-          const data = {
-            total_amount: Number(order.price), // from client
-            currency: "BDT",
-            tran_id: tran_id,
-            success_url: `http://localhost:3000/success/${tran_id}`,
-            fail_url:`http://localhost:3000/fail/${tran_id}` ,
-            cancel_url: "http://localhost:3030/cancel",
-            ipn_url: "http://localhost:3030/ipn",
+        const data = {
+          total_amount: Number(order.price), // from client
+          currency: "BDT",
+          tran_id: tran_id,
+          success_url: `http://localhost:3000/success/${tran_id}`,
+          fail_url: `http://localhost:3000/fail/${tran_id}`,
+          cancel_url: "http://localhost:3030/cancel",
+          ipn_url: "http://localhost:3030/ipn",
 
-            // Customer Info
-            cus_name: order.name,
-            cus_email:order.email ,
-            cus_add1:order.address ,
-            cus_add2: order.address,
-            cus_city: "Dhaka",
-            cus_state: "Dhaka",
-            cus_postcode: "1000",
-            cus_country: "Bangladesh",
-            cus_phone: "01700000000",
-            cus_fax: "01700000000",
+          // Customer Info
+          cus_name: order.name,
+          cus_email: order.email,
+          cus_add1: order.address,
+          cus_add2: order.address,
+          cus_city: "Dhaka",
+          cus_state: "Dhaka",
+          cus_postcode: "1000",
+          cus_country: "Bangladesh",
+          cus_phone: "01700000000",
+          cus_fax: "01700000000",
 
-            // Shipping Info
-            shipping_method: "Courier",
-            ship_name: order.name,
-            ship_add1: order.address,
-            ship_add2: order.address,
-            ship_city: "Dhaka",
-            ship_state: "Dhaka",
-            ship_postcode: 1000,
-            ship_country: "Bangladesh",
+          // Shipping Info
+          shipping_method: "Courier",
+          ship_name: order.name,
+          ship_add1: order.address,
+          ship_add2: order.address,
+          ship_city: "Dhaka",
+          ship_state: "Dhaka",
+          ship_postcode: 1000,
+          ship_country: "Bangladesh",
 
-            // Product Info
-            product_name: "Order Items",
-            product_category: "Mixed",
-            product_profile: "general",
-          };
-          
-
-        // Initialize SSLCommerz
-          const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
-          sslcz.init(data).then(async (apiResponse) => {
-
-
-            if (apiResponse?.GatewayPageURL) {
-                res.send({
-                url: apiResponse.GatewayPageURL,
-              });
-
-              const finalOrder={
-               ... order,
-                tran_id,
-                successStatus:false
-              }
-
-              await paymentCollection.insertOne(finalOrder);
-
-              const deleteQuery = {
-          _id: { $in: order.id.map((id) => new ObjectId(id)) },
+          // Product Info
+          product_name: "Order Items",
+          product_category: "Mixed",
+          product_profile: "general",
         };
 
-        await cartsCollection.deleteMany(deleteQuery);
+        // Initialize SSLCommerz
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+        sslcz.init(data).then(async (apiResponse) => {
+          if (apiResponse?.GatewayPageURL) {
+            res.send({
+              url: apiResponse.GatewayPageURL,
+            });
 
+            const finalOrder = {
+              ...order,
+              tran_id,
+              successStatus: false,
+            };
 
+            await paymentCollection.insertOne(finalOrder);
 
-            } else {
-              return res.status(400).send({
-                message: "Payment session failed!",
-              });
-            }
-          });
+            const deleteQuery = {
+              _id: { $in: order.id.map((id) => new ObjectId(id)) },
+            };
 
+            await cartsCollection.deleteMany(deleteQuery);
+          } else {
+            return res.status(400).send({
+              message: "Payment session failed!",
+            });
+          }
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).send({ error: err.message });
+      }
+    });
 
+    app.post("/success/:tran_id", async (req, res) => {
+      const tranId = req.params.tran_id;
 
-        } catch (err) {
-          console.log(err);
-          res.status(500).send({ error: err.message });
-        }
+      console.log("Payment Success:", tranId);
+
+      await paymentCollection.updateOne(
+        { tran_id: tranId },
+        { $set: { PaidStatus: true } }
+      );
+
+      res.send("Payment Success");
+    });
+
+    app.post("/fail/:tran_id", async (req, res) => {
+      const tranId = req.params.tran_id;
+
+      res.send("Payment fail");
+    });
+
+    app.get("/paymentHistory/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+
+        const query = { email: email };
+
+        const result = await paymentCollection.find(query).toArray();
+
+        res.status(200).json({ status: true, data: result });
+      } catch (err) {
+        res.status(500).json({ status: "fail", error: err });
+      }
+    });
+
+    app.get("/state", async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const order = await paymentCollection.estimatedDocumentCount();
+      const allProduct = await products.estimatedDocumentCount();
+
+      // Aggregate must use await!
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: { $sum: "$price" },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        order,
+        allProduct,
+        revenue,
       });
+    });
 
-app.post("/success/:tran_id", async (req, res) => {
-  const tranId = req.params.tran_id;
+    app.get("/orderState", async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$orderName",
+          },
+          {
+            $lookup: {
+              from: "products",
+              localField: "orderName",
+              foreignField: "name",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quentity: { $sum: 1 },
+              totalPrice: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quentity: 1,
+              totalPrice: 1,
+            },
+          },
+        ])
+        .toArray();
 
-  console.log("Payment Success:", tranId);
+      res.send(result);
+    });
 
-  await paymentCollection.updateOne(
-    { tran_id: tranId },
-    { $set: { PaidStatus: true } }
-  );
-
-  res.send("Payment Success");
-});
-
-app.post("/fail/:tran_id", async (req, res) => {
-
-  const tranId = req.params.tran_id;
-
-
-  res.send("Payment fail");
-
-
-
-});
-
-
-    
-
-app.get('/paymentHistory/:email', async( req, res)=>{
-
-
-  try{
-
-    const email=req.params.email;
-
-  const query={email : email};
-
-  const result=await paymentCollection.find(query).toArray();
-
-  res.status(200).json({status:true, data:result});
-
-  }catch(err){
-
-    res.status(500).json({status:'fail', error:err})
-
-  }
-
-
-})
-
-app.get('/state', async (req, res) => {
-  const users = await usersCollection.estimatedDocumentCount();
-  const order = await paymentCollection.estimatedDocumentCount();
-  const allProduct = await products.estimatedDocumentCount();
-
-  // Aggregate must use await!
-  const result = await paymentCollection.aggregate([
-    {
-      $group: {
-        _id: null,
-        totalRevenue: { $sum: "$price" }
-      }
-    }
-  ]).toArray();
-
-  const revenue = result.length > 0 ? result[0].totalRevenue : 0;
-
-  res.send({
-    users,
-    order,
-    allProduct,
-    revenue
-  });
-});
-
-
-app.get('/orderState', async (req, res) => {
-
-  const result = await paymentCollection.aggregate([
-    {
-      $unwind: "$orderName"
-    },
-    {
-      $lookup: {
-        from: "products",
-        localField: "orderName",
-        foreignField: "name",
-        as: "menuItems"
-      }
-    },
-    {
-      $unwind: "$menuItems"
-    },
-    {
-      $group: {
-        _id: "$menuItems.category",
-        quentity: { $sum: 1 },
-        totalPrice: { $sum: "$menuItems.price" }
-      }
-    },
-    {
-      $project: {
-        _id: 0,
-        category: "$_id",
-        quentity: 1,
-        totalPrice: 1
-      }
-    }
-  ]).toArray();
-
-  res.send(result);
-});
-
-
-
-
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+    app.listen(3000, () => console.log("Server running on port 3000"));
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
